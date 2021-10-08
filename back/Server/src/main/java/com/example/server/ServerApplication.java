e01ca51f4e09acfb40b30b7879cc6d407c913bbf
UgAr-study <uglovskij.au@gmail.com>
2021-10-08 21:21:27 +0300

    back updated

--- back/Server/src/main/java/com/example/server/ServerApplication.java
+++ back/Server/src/main/java/com/example/server/ServerApplication.java
@@ -3,11 +3,11 @@ package com.example.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ServerApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
-    }
-
-}
+//@SpringBootApplication
+//public class ServerApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ServerApplication.class, args);
+//    }
+//
+//}
