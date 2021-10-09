ba20223adc048bd5ab3d645083ab6b28e11996e6
shugaley <shugalei.niu@phystech.edu>
2021-10-10 01:05:25 +0300

    Add getByParams

--- back/Server/src/main/java/com/example/server/controller/DataInfoController.java
+++ back/Server/src/main/java/com/example/server/controller/DataInfoController.java
@@ -1,14 +1,18 @@
 package com.example.server.controller;
 
 import com.example.server.models.DataInfo;
+import com.example.server.models.Field;
 import com.example.server.service.DataInfoService;
-import com.example.server.service.FieldParseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 @RestController
@@ -29,6 +33,38 @@ public class DataInfoController {
     @GetMapping(value = "/index")
     public ResponseEntity<List<DataInfo>> getAll() {
         List<DataInfo> dataInfos = dataInfoService.readAll();
+
+        return dataInfos != null && !dataInfos.isEmpty()
+                ? new ResponseEntity<>(dataInfos, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(value = "/index")
+    public ResponseEntity<List<DataInfo>> getByParams(@RequestParam(value = "partner", required = false) String partner) {
+        String[] allParams = new String[]{partner};
+
+        for (String iAllParam : allParams) {
+            if (iAllParam == null || iAllParam.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        }
+
+        ResponseEntity<List<DataInfo>> responseEntityDataInfosAll = getAll();
+        if (responseEntityDataInfosAll.getStatusCode() != HttpStatus.OK)
+            return new ResponseEntity<>(responseEntityDataInfosAll.getStatusCode());
+        
+        List<DataInfo> dataInfosAll = getAll().getBody();
+        List<DataInfo> dataInfos = new ArrayList<>();
+        
+        for (DataInfo iDataInfosAll : dataInfosAll) {
+            for (Field iField : iDataInfosAll.getFields()) {
+                for (String iAllParam : allParams) {
+                    if (Objects.equals(iField.getName(), iAllParam)) {
+                        dataInfos.add(iDataInfosAll);
+                    }
+                }
+            }
+        }
 
         return dataInfos != null && !dataInfos.isEmpty()
                 ? new ResponseEntity<>(dataInfos, HttpStatus.OK)
