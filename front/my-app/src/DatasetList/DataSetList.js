67ce7dd3f669cae7baf1208fb6281726b08d8b2e
Minerkow <melnikov.ignat@phystech.edu>
2021-10-09 05:22:20 +0200

    updated

---
+++ front/my-app/src/DatasetList/DataSetList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import DatasetItem from "./DatasetItem"
+import PropTypes from "prop-types"
+
+const styles = {
+    ul: {
+        listStyle: 'none',
+        margin: 0,
+        padding: 0
+    }
+}
+
+function DatasetList(props) {
+    return(
+        <ul style={styles.ul}>
+            {
+                props.dataSetArr.map(ds => {
+                    return <DatasetItem dataset={ds} key={ds.id} selectDatasetFunc={props.selectDatasetFunc} 
+                                     selectFeaturesFunc={props.selectFeaturesFunc} />
+                })
+            }
+        </ul>
+    )
+}
+
+DatasetList.propTypes = {
+    dataSetArr: PropTypes.arrayOf(PropTypes.object).isRequired,
+    selectDatasetFunc: PropTypes.func.isRequired,
+    selectFeaturesFunc: PropTypes.func.isRequired,
+}
+
+export default DatasetList
---
+++ front/my-app/src/DatasetList/FeaturesItem.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types"
+
+function FeaturesItem(props) {
+    const classes = []
+
+    function checkSelected(fName) {
+        props.dataset.features.map(f => {
+            if (f.name === fName) {
+                console.log(f.isSelected)
+                return f.isSelected;
+            }
+        })
+    }
+
+    if (checkSelected(props.fName)) {
+        classes.push('fItemAfter')
+    } else {
+        classes.push('fItemBefore')
+    }
+
+    return(
+        <li >
+        <img className={classes.join(' ')} onClick={() => {props.selectFeaturesFunc(props.fName);}}></img>
+    </li>
+    )
+}
+
+FeaturesItem.propTypes = {
+    dataset: PropTypes.array,
+    fName: PropTypes.string,
+    selectFeaturesFunc: PropTypes.func.isRequired,
+}
+
+export default FeaturesItem 
